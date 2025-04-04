87a78d19e9bef84b83c6c2303431d87d4f60a080
Andrew Heberle <andrew.heberle@gmail.com>
2025-04-04 08:26:46 +0800

    Add additional example

--- example_test.go
+++ example_test.go
@@ -21,6 +21,29 @@ func ExampleBucket_Get() {
 		panic(err)
 	}
 
+	v := db.Get(key)
+	if v == nil {
+		panic("no value returned")
+	}
+	fmt.Printf("%s\n", v)
+	// Output: myvalue
+}
+
+func ExampleBucket_GetE() {
+	bucket := []byte("mybucket")
+	key := []byte("mykey")
+	value := []byte("myvalue")
+
+	db, err := ubolt.OpenBucket("database.db", bucket)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if err := db.Put(key, value); err != nil {
+		panic(err)
+	}
+
 	v, err := db.GetE(key)
 	if err != nil {
 		panic(err)
